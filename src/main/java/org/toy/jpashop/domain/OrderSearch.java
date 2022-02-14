f0c8e2907af1c89cadfb6794e0937c483755fa25
ShinGyeongseon <tlsrid1119@gmail.com>
2022-02-14 22:40:52 +0900

    주문 서비스 , 상품 서비스

---
+++ src/main/java/org/toy/jpashop/domain/OrderSearch.java
@@ -0,0 +1,2 @@
+package org.toy.jpashop.domain;public class OrderSearch {
+}
---
+++ src/main/java/org/toy/jpashop/exception/NotEnoughStockException.java
@@ -0,0 +1,5 @@
+package org.toy.jpashop.exception;
+
+public class NotEnoughStockException extends RuntimeException {
+    
+}
